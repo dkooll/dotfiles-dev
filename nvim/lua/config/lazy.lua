2f536e7f4b776b8b21d015c24728842478143724
dkooll <dennis.kool@cloudnation.nl>
2025-11-17 12:33:12 +0100

    fix packages

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,7 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local first_install = false
 if vim.fn.empty(vim.fn.glob(lazypath)) == 1 then
-  first_install = true
   vim.fn.system({
     "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", lazypath,
@@ -51,16 +49,15 @@ require("lazy").setup("plugins", {
   debug = false,
 })
 
--- Auto-quit after initial install
-if first_install then
+-- Show restart message after first install
+local first_run_flag = vim.fn.stdpath("data") .. "/.lazy-first-run"
+if vim.fn.filereadable(first_run_flag) == 0 then
   vim.api.nvim_create_autocmd("User", {
-    pattern = "VeryLazy",
+    pattern = "LazyDone",
     once = true,
     callback = function()
-      vim.notify("First install complete. Exiting in 3 seconds...", vim.log.levels.WARN)
-      vim.defer_fn(function()
-        vim.cmd("qall!")
-      end, 3000)
+      vim.fn.writefile({}, first_run_flag)
+      vim.cmd('echo "Installation complete. Please restart nvim."')
     end,
   })
 end
