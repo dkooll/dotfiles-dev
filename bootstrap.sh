5b04c95237b4c85c837980164e7ea7199c064639
dkooll <dennis.kool@cloudnation.nl>
2025-11-18 13:05:15 +0100

    refactor

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,12 @@ set -eu
 REPO_URL="https://github.com/dkooll/dotfiles-dev.git"
 TARGET_DIR="$HOME/workspaces/dkooll/dotfiles-dev"
 
+sudo -v
 sudo timedatectl set-ntp true 2>/dev/null || true
 
-command -v git &>/dev/null && command -v zsh &>/dev/null || sudo apt update && sudo apt install -y git zsh
-
+if ! command -v git &>/dev/null || ! command -v zsh &>/dev/null; then
+    sudo apt update && sudo apt install -y git zsh
+fi
 [[ "$SHELL" == *zsh ]] || sudo chsh -s "$(which zsh)" "$USER"
 
 mkdir -p "$(dirname "$TARGET_DIR")"
